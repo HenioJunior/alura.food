9cadec7b04a07c34463e12d962dc381ba0c94669
Henio Junior <henioajunior@gmail.com>
2023-03-23 16:20:36 -0300

    Refactoring | Removido o lombok e impl de um bean para o ModelMapper

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -1,15 +1,104 @@
 package br.com.alurafood.pagamentos.dto;
 
 import br.com.alurafood.pagamentos.enums.Status;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-public record PagamentoDto(
-        BigDecimal valor, String nome, String numero, String expiracao, String codigo, Status status, Long pedidoId,
-        Long formaDePagamentoId
-) {
+
+public final class PagamentoDto {
+    private Long id;
+    private BigDecimal valor;
+    private String nome;
+    private String numero;
+    private String expiracao;
+    private String codigo;
+    private Status status;
+    private Long pedidoId;
+    private Long formaDePagamentoId;
+
+    public PagamentoDto(
+            BigDecimal valor, String nome, String numero, String expiracao, String codigo, Status status, Long pedidoId,
+            Long formaDePagamentoId
+    ) {
+        this.valor = valor;
+        this.nome = nome;
+        this.numero = numero;
+        this.expiracao = expiracao;
+        this.codigo = codigo;
+        this.status = status;
+        this.pedidoId = pedidoId;
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(String expiracao) {
+        this.expiracao = expiracao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Long getFormaDePagamentoId() {
+        return formaDePagamentoId;
+    }
+
+    public void setFormaDePagamentoId(Long formaDePagamentoId) {
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
 }
--- pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -1,10 +1,6 @@
 package br.com.alurafood.pagamentos.model;
 
 import br.com.alurafood.pagamentos.enums.Status;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -15,12 +11,7 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "pagamentos")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Pagamento {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -56,4 +47,89 @@ public class Pagamento {
     @NotNull
     private Long formaDePagamentoId;
 
+    public Pagamento() {
+    }
+
+    public Pagamento(BigDecimal valor, String nome, String numero, String expiracao, String codigo, Status status, Long pedidoId, Long formaDePagamentoId) {
+        this.valor = valor;
+        this.nome = nome;
+        this.numero = numero;
+        this.expiracao = expiracao;
+        this.codigo = codigo;
+        this.status = status;
+        this.pedidoId = pedidoId;
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(String expiracao) {
+        this.expiracao = expiracao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Long getFormaDePagamentoId() {
+        return formaDePagamentoId;
+    }
+
+    public void setFormaDePagamentoId(Long formaDePagamentoId) {
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
 }
